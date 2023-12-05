af82b3e7b1cc75aac523d079921113c8c6655cda
Mokammel Tanvir <mokammeltanvir01@gmail.com>
2023-12-06 02:53:56 +0600

    pagination

--- resources/views/frontend/pages/venue-list.blade.php
+++ resources/views/frontend/pages/venue-list.blade.php
@@ -77,22 +77,10 @@
 
         </div>
         <div class="row">
-            {{ $allvenue->links() }}
             <div class="col-md-12 tp-pagination">
-                <ul class="pagination">
-                    <li>
-                        <a href="#" aria-label="Previous"> <span aria-hidden="true">Previous</span> </a>
-                    </li>
-                    <li class="active"><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">5</a></li>
-                    <li>
-                        <a href="#" aria-label="Next"> <span aria-hidden="true">NEXT</span> </a>
-                    </li>
-                </ul>
+                {{ $allvenue->links() }}
             </div>
+
         </div>
     </div>
 </div>
